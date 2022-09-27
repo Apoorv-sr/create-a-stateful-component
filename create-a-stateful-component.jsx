39f6a4fd02d198d896e5e4210900b1fc40809d72
Apoorv-sr <109285079+Apoorv-sr@users.noreply.github.com>
2022-09-27 16:31:58 +0530

    Update create-a-stateful-component.jsx

--- create-a-stateful-component.jsx
+++ create-a-stateful-component.jsx
@@ -4,6 +4,7 @@ class StatefulComponent extends React.Component {
     // Only change code below this line
     //see we already know the importance of state in react,state property is used to detect and render if there is any change done to the component i.e see here Stateful is the component name and if we want to declare its state then we have to declare it in the constructor like it is shown i.e. this.state={} i.e. it has to be created like a javascript object and the keys in the object are the state variables and if they are rendered on screen and if any change is done on the rendered variale inside the application then the change would be redered automatically and we need not refresh the page so this is the importance that we can traget a specific component outof many components being rendered on the screen and update and render only that component using the state property and state for a component stays till the lifecycle of that component
     //and also note that we need class component to declare state  
+    //Note that If you want to access a state value within the return of the render method, you have to enclose the value in curly braces. but if you want to access outside of return in render method then that can be treated as a normal javascript and curly braces not required.
     this.state={
         firstName:"Apoorva Singh"
       }
